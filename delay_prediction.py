a302b96476c1084ed0dc6524b3d8a57687bb9e99
plazer1 <computermaster360@gmail.com>
2016-10-05 19:59:41 +0200

    upravene pre "delays_dataset.csv"

--- delay_prediction.py
+++ delay_prediction.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python2
 
 from __future__ import print_function
+import sys
 import numpy as np
 from pprint import pprint
 import csv
 from datetime import datetime
 
+apts_fname = 'airports.csv'
+in_fname = 'delays_dataset.csv'
+datefmt = '%Y-%m-%d %H:%M:%S'
+
+
 airport2gps = {}
-with open('airports.csv') as airport_csvfile:
-    reader = csv.reader(airport_csvfile)
+with open(apts_fname) as apts_csvfile:
+    reader = csv.reader(apts_csvfile)
     for row in reader:
         a_code = row[4]
         if a_code:
@@ -18,10 +24,13 @@ with open('airports.csv') as airport_csvfile:
 
 
 linecount = 0
-with open('2008.csv') as data_file:
-    for line in data_file:
-        linecount += 1
-linecount = 1000000
+'''with open(in_fname) as data_file:
+    for linecount,line in enumerate(data_file):
+        pass
+'''
+linecount = 10000
+# read first 100 lines, compute avg line len;
+# linecount = int( 1.02 * (filesize / avg line len) )
 
 
 carriers_id = np.chararray(linecount, 3)
@@ -34,51 +43,41 @@ delays = np.zeros(linecount, dtype=np.int16)
 carriers_set = set()
 counter = 0
 
-with open('test_flight_info.csv') as data_file:
+with open(in_fname) as data_file:
     reader = csv.DictReader(data_file)
-    for i, row in enumerate(reader):
+    i = 0
+    for row in reader:
         if i == linecount:
             break
         try:
-            carrier = row['UniqueCarrier']
+            carrier = row['carrier']
             carriers_set.add(carrier)
 
-            day   = int(row['DayofMonth'])
-            month = int(row['Month'])
-            year  = int(row['Year'])
-
-            sch_dep_time = row['CRSDepTime']
-            hr, mn = int(sch_dep_time[:-2]), int(sch_dep_time[-2:])
-            schdep = datetime(year, month, day, hr, mn)
-
-            act_dep_time = row['DepTime']
-            hr, mn = int(act_dep_time[:-2]), int(act_dep_time[-2:])
-            actdep = datetime(year, month, day, hr, mn)
+            schdep = datetime.strptime(row['scheduled_departure'], datefmt)
+            actdep = datetime.strptime(row['actual_departure'],    datefmt)
 
             delay = int( (actdep - schdep).total_seconds() / 60 )
-            if delay < -12*60:
-                delay += 24*60
 
             carriers_id[i] = carrier
-            flight_numbers[i] = int(row['FlightNum'])
-            orig[i] = airport2gps[row['Origin']]
-            dest[i] = airport2gps[row['Dest']]
+            flight_numbers[i] = int(row['fltno'])
+            orig[i] = airport2gps[row['dep_apt']]
+            dest[i] = airport2gps[row['arr_apt']]
             date[i] = schdep.month, schdep.day, schdep.isoweekday(), schdep.hour * 60 + schdep.minute
             delays[i] = delay
+            i += 1
 
-        except:
-            counter += 1
+        except Exception:
+            pass
 
-print('Error count:', counter)
+print('Error count:', linecount - i)
 
 
 carrier_id_to_idx = {cid:idx for idx,cid in enumerate(carriers_set)}
 n_carriers = len(carriers_set)
 del carriers_set
 
-
 def make_entry(i):
-    carrirer_idx = carrier_id_to_idx[carriers_id[i]]
+    carrier_idx = carrier_id_to_idx[carriers_id[i]]
     # one-hot carrier
     res = [0] * n_carriers
     res[carrier_idx] = 1
@@ -88,8 +87,10 @@ def make_entry(i):
     res.extend(dest[i])
     res.extend(date[i])
 
-    return np.array(res, dtype=np.float32), delays[i]
+    return np.array(res, dtype=np.float32), delays[i]  # float err !
 
 
-pprint(carriers)
+for i in range(10):
+    e,d = make_entry(i)
+    print(i+1, e[n_carriers:])
 
